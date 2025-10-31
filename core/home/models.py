097d5dabb42aca479281546faf6b093ee5b6402c
jmkihahu <jmkihahu@github.com>
2025-10-31 14:14:31 +0300

    core

---
+++ core/home/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Expense(models.Model):
+    user = models.ForeignKey(User,
+    on_delete=models.SET_NULL,null=True,blank=True)
+    salary = models.IntegerField(default=0)
+    name = models.CharField(max_length=100,
+                            default="something")
+    price = models.IntegerField(default=0)
+    
